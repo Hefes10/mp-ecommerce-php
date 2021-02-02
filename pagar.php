468bd2215cdaf1b90f7c71031bf6c12f0809913b
Martin Miret <martin.miret@gmail.com>
2021-02-02 19:22:03 -0300

    prueba1

---
+++ pagar.php
@@ -0,0 +1,36 @@
+<?php
+    require_once 'vendor/autoload.php'; // You have to require the library from your Composer vendor folder
+
+    MercadoPago\SDK::setAccessToken("APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398"); // Either Production or SandBox AccessToken
+
+    $payment = new MercadoPago\Payment();
+    
+    $payment->transaction_amount = 141;
+    $payment->token = "YOUR_CARD_TOKEN";
+    $payment->description = "Ergonomic Silk Shirt";
+    $payment->installments = 1;
+    $payment->payment_method_id = "visa";
+    $payment->payer = array(
+      "email" => "larue.nienow@email.com"
+    );
+
+    $payment->save();
+
+    echo $payment->status;
+  ?>
+
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Document</title>
+  </head>
+  <body>
+      
+<script
+  src="https://www.mercadopago.com.ar/integrations/v1/web-payment-checkout.js"
+  data-preference-id="<?php echo $preference->id; ?>">
+</script>
+  </body>
+  </html>
