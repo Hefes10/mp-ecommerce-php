9c44decf40e6997c662eed360b8672d45961b548
Hefes <martin.miret@gmail.com>
2021-02-04 19:42:00 -0300

    prueba medios de pago excluidos 1.5

--- detail.php
+++ detail.php
@@ -17,10 +17,10 @@ $item->unit_price = $_POST['price'];
 $preference->items = array($item);
 $preference->payment_methods = array(
     "excluded_payment_methods" => array(
-      array("credit_card" => "master")
+      array("id" => "amex")
     ),
     "excluded_payment_types" => array(
-      array("ticket" => "pagofacil")
+      array("id" => "pagofacil")
     ),
     "installments" => 6
   );
