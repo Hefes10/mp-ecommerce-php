8e0e2a26170a49e7f57b788e36b8d2039a2524bd
Martin Miret <martin.miret@gmail.com>
2021-02-06 14:35:50 -0300

    detalle del item

--- pagar.php
+++ pagar.php
@@ -1,4 +1,3 @@
-
 <?php
 // SDK de Mercado Pago
 require __DIR__ .  '/vendor/autoload.php';
@@ -11,35 +10,38 @@ $preference = new MercadoPago\Preference();
 
 // Crea un ítem en la preferencia
 $item = new MercadoPago\Item();
-$item->title = 'Pantalon';
+$item->id = "1234";
+$item->title = $_POST['title'];
+$item->description = "Dispositivo móvil de Tienda e-commerce";
 $item->quantity = 1;
-$item->unit_price = 12;
+$item->unit_price = $_POST['price'];
+$item->currency_id = "ARS";
 $preference->items = array($item);
 $preference->save();
 ?>
 
 
-  <!DOCTYPE html>
-  <html lang="en">
-  <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Document</title>
-  </head>
-  <body>
-      
-<form action="insertarpago.php" method="post">
-<script
-  src="https://www.mercadopago.com.ar/integrations/v1/web-payment-checkout.js"
-  data-preference-id="<?php echo $preference->id; ?>">
-</script>
-</form>
-  </body>
-  </html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
 
-  <!-- curl -X POST -H "Content-Type: application/json" -H 'Authorization: Bearer TEST-1371304897199119-020420-a6437db78b3173cdaa998f3e4de1d45b-187689351' "https://api.mercadopago.com/users/test_user" -d '{"site_id":"MLA"}' -->
+<body>
 
-  <!-- {
+  <form action="insertarpago.php" method="post">
+    <script src="https://www.mercadopago.com.ar/integrations/v1/web-payment-checkout.js" data-preference-id="<?php echo $preference->id; ?>">
+    </script>
+  </form>
+</body>
+
+</html>
+
+<!-- curl -X POST -H "Content-Type: application/json" -H 'Authorization: Bearer TEST-1371304897199119-020420-a6437db78b3173cdaa998f3e4de1d45b-187689351' "https://api.mercadopago.com/users/test_user" -d '{"site_id":"MLA"}' -->
+
+<!-- {
     "id":711079897,
     "nickname":"TETE84500",
     "password":"qatest6814",
@@ -47,7 +49,7 @@ $preference->save();
     "email":"test_user_50359375@testuser.com"
     } 
     vendedor -->
-  <!-- {
+<!-- {
     "id":711081245,
     "nickname":"TESTNBLOYBJC",
     "password":"qatest8241",
